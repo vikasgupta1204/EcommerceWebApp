f34933442d2619a04ab487f9139a3193ec1042e9
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2024-03-04 06:59:16 +0530

    added documentation for api's

--- UserService/src/main/java/com/ecom/userservice/controller/RoleController.java
+++ UserService/src/main/java/com/ecom/userservice/controller/RoleController.java
@@ -2,12 +2,12 @@ package com.ecom.userservice.controller;
 
 import com.ecom.userservice.model.Role;
 import com.ecom.userservice.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/role")
@@ -20,20 +20,44 @@ public class RoleController {
         this.roleService = roleService;
     }
 
+    @Operation(summary = "Add a new role")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Role added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "409", description = "Role already exists")
+    })
     @PostMapping("/add")
     public ResponseEntity<Role> addRole(@RequestParam String roleName) {
         return ResponseEntity.ok(roleService.addRole(roleName));
     }
 
+    @Operation(summary = "Update a role")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Role updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Role not found")
+    })
     @PostMapping("/update")
     public ResponseEntity<Role> updateRole(@RequestParam long roleId,@RequestParam String roleName) {
         return ResponseEntity.ok(roleService.updateRole(roleId, roleName));
     }
+
+    @Operation(summary = "Delete a role by name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Role deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Role not found")})
+
     @PostMapping("/deleteByName")
     public ResponseEntity<Role> deleteRoleByName(@RequestParam String roleName) {
         return ResponseEntity.ok(roleService.deleteRoleByName(roleName));
     }
 
+    @Operation(summary = "Delete a role by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Role deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Role not found")})
     @PostMapping("/deleteById")
     public ResponseEntity<Role> deleteRoleById(@RequestParam long roleId) {
         return ResponseEntity.ok(roleService.deleteRoleById(roleId));
--- UserService/src/main/java/com/ecom/userservice/controller/UserController.java
+++ UserService/src/main/java/com/ecom/userservice/controller/UserController.java
@@ -4,10 +4,11 @@ import com.ecom.userservice.dtos.LoginRequestDto;
 import com.ecom.userservice.dtos.LoginResponseDto;
 import com.ecom.userservice.dtos.SignUpRequestDto;
 import com.ecom.userservice.dtos.SignUpResponseDto;
-import com.ecom.userservice.model.Token;
 import com.ecom.userservice.model.User;
 import com.ecom.userservice.service.UserService;
-import lombok.extern.java.Log;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,20 +22,46 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @Operation(summary = "Sign up a new user")
+    @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "User signed up successfully"),
+           @ApiResponse(responseCode = "400", description = "Invalid input"),
+           @ApiResponse(responseCode = "409", description = "User already exists")
+    })
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         return userService.signUp(signUpRequestDto.getName(),signUpRequestDto.getEmail(), signUpRequestDto.getPassword(), signUpRequestDto.getRoles());
     }
+
+    @Operation(summary = "Login a user")
+    @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "User logged in successfully"),
+              @ApiResponse(responseCode = "400", description = "Invalid input"),
+              @ApiResponse(responseCode = "401", description = "Unauthorized")
+     })
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
         return userService.login(loginRequestDto);
     }
 
+    @Operation(summary = "Logout a user")
+    @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "User logged out successfully"),
+                @ApiResponse(responseCode = "400", description = "Invalid input"),
+                @ApiResponse(responseCode = "401", description = "Unauthorized")
+         })
     @PostMapping("/logout")
     public ResponseEntity<String> logout(@RequestParam String token) {
         return userService.logout(token);
     }
 
+    @Operation(summary = "Validate a token")
+    @ApiResponses(value = {
+           @ApiResponse(responseCode = "200", description = "Token is valid"),
+                @ApiResponse(responseCode = "400", description = "Invalid input"),
+                @ApiResponse(responseCode = "401", description = "Unauthorized")
+         })
     @PostMapping("/validateToken")
     public ResponseEntity<User> validateToken(@RequestParam("token") String token) {
         return userService.validateToken(token);
